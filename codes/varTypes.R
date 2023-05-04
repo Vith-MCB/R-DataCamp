25508bbd5e30834abdbad84d3b84f4b92268fd0d
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-05-04 16:24:30 -0300

    unc code

--- codes/varTypes.R
+++ codes/varTypes.R
@@ -1,25 +1,3 @@
-#Base Code
-# Change my_numeric to be 42
-my_numeric <- 42.5
-
-# Change my_character to be "universe"
-my_character <- "some text"
-
-# Change my_logical to be FALSE
-my_logical <- TRUE
-
-
----- CHALLANGE ----
-
-Change the value of the:
-
-my_numeric variable to 42.
-my_character variable to "universe". Note that the quotation marks indicate that "universe" is a character.
-my_logical variable to FALSE.
-Note that R is case sensitive!
-
-____________________
-
 # Change my_numeric to be 42
 my_numeric <- 42
 
